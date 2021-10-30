513dff145dac83465f9e07628ff1893a7a05569a
wanrabbae <wancode.msg@gmail.com>
2021-10-30 11:29:24 +0700

    Fix some bugs

--- client/src/apis/serverApi.js
+++ client/src/apis/serverApi.js
@@ -1,9 +1,17 @@
 import axios from 'axios';
+import Cookies from 'js-cookie';
 
 const api = axios.create({
-    baseURL: "http://localhost:4000/api",
+    baseURL: 'http://localhost:4000/api',
     withCredentials: true,
-    credentials: "include"
+})
+// kirim setiap request ke server dengan header Authorization yang berisi token
+api.interceptors.request.use((req) => {
+    const token = Cookies.get('token');
+    if (token) {
+        req.headers.Authorization = `${token}`;
+    }
+    return req;
 })
 
 export default api;
--- server/src/middlewares/auth.js
+++ server/src/middlewares/auth.js
@@ -1,8 +1,7 @@
 const jwt = require('jsonwebtoken');
 
 exports.auth = (req, res, next) => {
-    const token = req.cookies.token
-
+    const token = req.header('Authorization');
     if (!token) {
         return res.status(401).json({
             status: 'failed',
