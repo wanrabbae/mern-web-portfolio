60cde561a8d8efb88ed4c4385b119fd107ab3f96
wanrabbae <wancode.msg@gmail.com>
2021-11-01 19:57:20 +0700

    Fix bug CORS

--- client/src/apis/serverApi.js
+++ client/src/apis/serverApi.js
@@ -1,10 +1,13 @@
 import axios from 'axios';
 import Cookies from 'js-cookie';
 
+const environment = 'production' // change this if you want to test on production
+
 const api = axios.create({
-    baseURL: process.env.REACT_APP_ENVIRONMENT === 'development' ? 'http://localhost:4000/api' : process.env.REACT_APP_SERVER_URL,
+    baseURL: environment === 'development' ? 'http://localhost:4000/api' : process.env.REACT_APP_SERVER_URL,
     withCredentials: true,
 });
+
 // kirim setiap request ke server dengan header Authorization yang berisi token
 api.interceptors.request.use((req) => {
     const token = Cookies.get('token');
