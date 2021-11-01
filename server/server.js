7f977fa5836a9e543e1e98e6399b6712cf11ce39
wanrabbae <wancode.msg@gmail.com>
2021-11-01 19:57:47 +0700

    Fix bug CORS

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ require('./config/db.js');
 
 app.use(helmet());
 app.use(cors({
-    origin: process.env.ENVIRONMENT === 'development' ? 'http://localhost:3000' : 'https://alwan.vercel.app/',
+    origin: process.env.ENVIRONMENT === 'development' ? 'http://localhost:3000' : 'https://alwan.vercel.app',
     credentials: true
 }));
 app.use(express.json());
